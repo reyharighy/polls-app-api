2431b79a2fdb590637b4ff608e2bb8d090208acf
reyharighy <arighymoch@gmail.com>
2025-08-28 16:15:12 +0700

    Provide member sets for the poll model stored in redis
    Details:
    1. We should provide set of members when saving a vote in redis, including vote_ids and voter's email
    2. It would speed up the process of checking if the vote_id is correct and the voter makes the first attempt to vote, without having to reiterate through the each model's attributes.
    3. Use regex to filter out when fetching all polls data in redis that's a type of set of members.

--- app/services/utils.py
+++ app/services/utils.py
@@ -1,6 +1,7 @@
 """Module to provide some utilities to the application."""
 
 import os
+import re
 from uuid import UUID
 from typing import List
 from redis import Redis
@@ -24,7 +25,7 @@ def save_poll(poll: Poll):
 
 def get_poll(poll_id: UUID, indexing: bool = False) -> Poll:
     """Method to retrieve poll data from redis given the specified poll_id"""
-    poll_json = redis_client.get(f"poll:{poll_id}")
+    poll_json = redis_client.get(name=f"poll:{poll_id}")
 
     if not indexing and not poll_json:
         raise HTTPException(
@@ -34,24 +35,25 @@ def get_poll(poll_id: UUID, indexing: bool = False) -> Poll:
 
     return Poll.model_validate_json(poll_json) # type: ignore
 
-def check_poll_is_active(poll: Poll):
-    """Check if the poll is still active given the specified poll_id from path parameter"""
+def validate_poll_active(poll: Poll):
+    """Validate if the poll is still active"""
     if not poll.is_active():
         raise HTTPException(
             status_code=410,
             detail={"msg": f"Poll of id {poll.id} has expired"}
         )
 
-def get_option_description(poll: Poll, option_id: UUID) -> str:
-    """
-    Get the option description with the following requirements:
-    1. Check if the given poll is still active.
-    2. Check if the option_id is valid given the specified poll.
-    3. If option is valid, return the option description.
-    """
+def validate_voter(poll_id: UUID, email: str):
+    """Validate if the current voter makes the first vote"""
+    if redis_client.sismember(f"poll:{poll_id}:emails", email):
+        raise HTTPException(
+            status_code=409,
+            detail={"msg": f"Voter of email {email} has already voted in poll of id {poll_id}"}
+        )
 
-    check_poll_is_active(poll=poll)
-    options = {choice.id:choice.description for choice in poll.options}
+def get_option_description(poll: Poll, option_id: UUID) -> str:
+    """Get the option description given the specified poll and option_id"""
+    options = {option.id:option.description for option in poll.options}
     option_description = options.get(option_id)
 
     if not option_description:
@@ -65,16 +67,26 @@ def get_option_description(poll: Poll, option_id: UUID) -> str:
 def get_all_polls() -> List[Poll]:
     """Method to retrieve all polls data from redis"""
     all_polls = []
+    poll_only_pattern = re.compile(r"^poll:[A-Fa-f0-9-]+$")
 
-    _, keys = redis_client.scan(match="poll:*") # type: ignore
+    while True:
+        cursor, keys = redis_client.scan(match="poll:*") # type: ignore
 
-    for key in keys:
-        poll = get_poll(
-            poll_id=str(key).replace("poll:", ""), # type: ignore
-            indexing=True
-        )
+        filtered_keys: List[UUID] = [
+            str(key).removeprefix("poll:")
+            for key in keys if poll_only_pattern.match(key)
+        ] # type: ignore
 
-        all_polls.append(poll)
+        for key in filtered_keys:
+            poll = get_poll(
+                poll_id=key,
+                indexing=True
+            )
+
+            all_polls.append(poll)
+
+        if cursor == 0:
+            break
 
     return all_polls
 
@@ -82,7 +94,8 @@ def save_vote(vote: Vote):
     """Method to save vote data into redis"""
     vote_json = vote.model_dump_json()
     redis_client.set(f"vote:{vote.id}", vote_json)
-    redis_client.sadd(f"poll:{vote.poll.id}:voters", vote.voter.email)
+    redis_client.sadd(f"poll:{vote.poll.id}:vote_ids", str(vote.id))
+    redis_client.sadd(f"poll:{vote.poll.id}:emails", vote.voter.email)
 
 def get_vote(poll_id: UUID, vote_id: UUID, indexing: bool = False) -> Vote:
     """Method to retrieve vote data from redis given the specified vote_id"""
@@ -94,38 +107,27 @@ def get_vote(poll_id: UUID, vote_id: UUID, indexing: bool = False) -> Vote:
             detail={"msg": f"Vote of id {vote_id} is not found"}
         )
 
-    vote = Vote.model_validate_json(vote_json) # type: ignore
-
-    if not indexing and vote.poll.id != poll_id:
+    if not indexing and not redis_client.sismember(f"poll:{poll_id}:vote_ids", str(vote_id)):
         raise HTTPException(
             status_code=409,
             detail={"msg": f"Vote of id {vote_id} does not belong to poll of id {poll_id}"}
         )
 
-    return vote
+    return Vote.model_validate_json(vote_json) # type: ignore
 
 def get_all_votes(poll_id: UUID) -> List[Vote]:
     """Method to retrieve all votes data from redis"""
     all_votes = []
 
-    _, keys = redis_client.scan(match="vote:*") # type: ignore
+    vote_ids: List[UUID] = redis_client.smembers(f"poll:{poll_id}:vote_ids") # type: ignore
 
-    for key in keys:
+    for vote_id in vote_ids:
         vote: Vote = get_vote(
             poll_id=poll_id,
-            vote_id=key.replace("vote:", ""),
+            vote_id=vote_id,
             indexing=True
         )
 
-        if vote.poll.id == poll_id:
-            all_votes.append(vote)
+        all_votes.append(vote)
 
     return all_votes
-
-def validate_voter(poll_id: UUID, email: str):
-    """Validate if the current voter makes the first vote"""
-    if redis_client.sismember(f"poll:{poll_id}:voters", email):
-        raise HTTPException(
-            status_code=409,
-            detail={"msg": f"Voter of email {email} has already voted in poll of id {poll_id}"}
-        )
